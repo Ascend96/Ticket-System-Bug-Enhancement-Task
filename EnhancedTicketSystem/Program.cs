42b90a351921063725cb360ec195f22d2b8bfa64
Ascend96 <mmartens28@yahoo.com>
2020-10-16 16:51:06 -0500

    Add NLog - dotnet add package NLog.Web.AspNetCore - Create static nlog instance - Configure nlog targets

--- EnhancedTicketSystem/Program.cs
+++ EnhancedTicketSystem/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
 namespace EnhancedTicketSystem
 {
     class Program
     {
+        // create static instance of logger
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+
+            logger.Info("Program Started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program has ended");
         }
     }
 }
