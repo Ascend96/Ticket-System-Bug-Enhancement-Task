0a7f1be92545138b6f49ad03e4bebcd7d99552c2
Ascend96 <mmartens28@yahoo.com>
2020-10-16 19:36:01 -0500

    Menu - add new tickets by category - display all tickets by category

--- EnhancedTicketSystem/Program.cs
+++ EnhancedTicketSystem/Program.cs
@@ -13,7 +13,30 @@ namespace EnhancedTicketSystem
 
             logger.Info("Program Started");
 
-            Console.WriteLine("Hello World!");
+            // string ticketFilePath = "ticketFilePath.csv";
+
+            // ticket object code goes here
+
+            string choice = "";
+
+            // do while loop for menu and option selection
+
+            do{
+                // add/display tickets by category
+                Console.WriteLine("1) Add new Bug/Defect Ticket");
+                Console.WriteLine("2) Add new Enhancement Ticket");
+                Console.WriteLine("3) Add new Task Ticket");
+                Console.WriteLine("4) Display all Bug/Defect Tickets");
+                Console.WriteLine("5) Display all Enhancement Tickets");
+                Console.WriteLine("6) Display all Task Tickets");
+                
+                choice = Console.ReadLine();
+
+
+
+                
+
+            } while(choice == "1" || choice == "2" || choice == "3" || choice == "4" || choice == "5" || choice == "6");
 
             logger.Info("Program has ended");
         }
